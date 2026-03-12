7ef1b2da8e5719775212746784e643ca22d29d34
CarlosKurios <crodriguez.kuriosedu@gmail.com>
2026-03-11 22:10:31 -0400

    feat: refine admin and API views.

--- apps/scripts/admin.py
+++ apps/scripts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import ScriptTemplate, RouterScriptExecution
+
 # Register your models here.
+class ScriptTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'content')
+    list_filter = ('name', 'description')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
+
+class RouterScriptExecutionAdmin(admin.ModelAdmin):
+    list_display = ('router', 'template', 'status', 'created_at', 'completed_at')
+    list_filter = ('router', 'template', 'status')
+    search_fields = ('router', 'template', 'status')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'completed_at')
+    
+
+admin.site.register(ScriptTemplate, ScriptTemplateAdmin)
+admin.site.register(RouterScriptExecution, RouterScriptExecutionAdmin)
--- apps/scripts/views.py
+++ apps/scripts/views.py
@@ -2,13 +2,13 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from .models import ScriptTemplate, RouterScriptExecution
 from .serializers import ScriptTemplateSerializer, RouterScriptExecutionSerializer
 
-class ScriptTemplateViewSet(ModelViewSet):
+class ScriptTemplateViewSet(ReadOnlyModelViewSet):
     queryset = ScriptTemplate.objects.all()
     serializer_class = ScriptTemplateSerializer
     permission_classes = [IsAuthenticated]
